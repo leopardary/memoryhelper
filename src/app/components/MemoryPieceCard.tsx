6b3c2b5e2c227c05515d7abb693576fe205d8570
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-03-24 21:48:47 -0700

    other pages/components

---
+++ src/app/components/MemoryPieceCard.tsx
@@ -0,0 +1,39 @@
+import { MemoryPiece } from "@prisma/client";
+import Link from "next/link";
+import Image from "next/image";
+
+interface MemoryPieceProps {
+  memoryPiece: MemoryPiece
+}
+export default function MemoryPieceCard({ memoryPiece }: MemoryPieceProps) {
+  const { id, description, content, subject, unit, labels, imageUrl } = memoryPiece;
+  return (
+    <Link
+      href={"/memorypiece/" + id}
+      className="card w-full bg-base-100 hover:shadow-xl transition-shadow"
+    >
+      <div className="m-4">
+        <figure>
+          <Image
+            src={imageUrl}
+            alt={content}
+            width={400}
+            height={400}
+            className="h-48 object-contain"
+          />
+        </figure>
+      </div>
+      <div className="card-body">
+        <h2 className="card-title">
+          {content}
+        </h2>
+        <p>{description}</p>
+        <div className="flex flex-wrap gap-2 mt-2">
+          {labels.map((label, index) => (
+            <div key={index} className="badge badge-outline">{label}</div>
+          ))}
+        </div>
+      </div>
+    </Link>
+  );
+}
