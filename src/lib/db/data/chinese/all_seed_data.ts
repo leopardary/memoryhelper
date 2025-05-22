2e7a60f9a27e72f86ff0d21c3197617d65a40f10
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-05-21 22:17:37 -0700

    include 4a3 for seed data

--- src/lib/db/data/chinese/all_seed_data.ts
+++ src/lib/db/data/chinese/all_seed_data.ts
@@ -1,9 +1,11 @@
 import data_4a1 from './seed_4a1'
 import data_4a2 from './seed_4a2'
+import data_4a3 from './seed_4a3'
 
 const data = {
   data_4a1: data_4a1,
-  data_4a2: data_4a2
+  data_4a2: data_4a2,
+  data_4a3: data_4a3
 }
 
 export default data;
