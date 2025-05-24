cfd85acbfdd5e9142d0b9ced0830c349e34c11ab
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-05-24 11:14:27 -0700

    removing validation for NEXTAUTH_URL and NEXTAUTH_SECRET

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,8 +4,6 @@ const envSchema = zod.object({
   DATABASE_URL: zod.string().nonempty(),
   GOOGLE_CLIENT_ID: zod.string().nonempty(),
   GOOGLE_CLIENT_SECRET: zod.string().nonempty(),
-  NEXTAUTH_URL: zod.string().nonempty(),
-  NEXTAUTH_SECRET: zod.string().nonempty(),
   IMAGE_BASE_PATH: zod.string().nonempty(),
 });
 
