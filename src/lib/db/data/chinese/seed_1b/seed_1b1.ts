e23f4cc1537326b0db53c03f45172d0fd5759188
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-05-27 20:54:20 -0700

    adding characters for chinese 1b unit1 lesson1

--- src/lib/db/data/chinese/seed_1b/seed_1b1.ts
+++ src/lib/db/data/chinese/seed_1b/seed_1b1.ts
@@ -16,7 +16,162 @@ const data = {
             description: '第一课，春夏秋冬',
             imageUrls: [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/lesson1a.jpeg`, `${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/lesson1b.jpeg`],
             order: 1,
-            data: []
+            data: [
+  {
+    "霜": {
+      "labels": ["会认字"],
+      "组词": ["霜冻", "霜降", "冰霜"],
+      "造句": [
+        "深秋的早晨，草地上铺满银白的霜冻。",
+        "二十四节气中的'霜降'表示天气渐冷。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/霜.gif`]
+    }
+  },
+  {
+    "吹": {
+      "labels": ["会认字"],
+      "组词": ["吹风", "吹拂", "吹奏"],
+      "造句": [
+        "春风轻轻吹拂着柳枝。",
+        "他正在学习吹奏萨克斯管。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/吹.gif`]
+    }
+  },
+  {
+    "落": {
+      "labels": ["会认字"],
+      "组词": ["落叶", "降落", "日落"],
+      "造句": [
+        "秋天的落叶像蝴蝶一样飞舞。",
+        "飞机平稳地降落在跑道上。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/落.gif`]
+    }
+  },
+  {
+    "降": {
+      "labels": ["会认字"],
+      "组词": ["降落", "降温", "降雨"],
+      "造句": [
+        "气象台预报明天将大幅降温。",
+        "久旱逢甘霖，这场降雨解了旱情。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/降.gif`]
+    }
+  },
+  {
+    "飘": {
+      "labels": ["会认字"],
+      "组词": ["飘雪", "飘带", "飘扬"],
+      "造句": [
+        "窗外开始飘雪，大地渐渐变白。",
+        "五星红旗在风中高高飘扬。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/飘.gif`]
+    }
+  },
+  {
+    "游": {
+      "labels": ["会认字"],
+      "组词": ["游泳", "游玩", "旅游"],
+      "造句": [
+        "夏天我们经常去河边游泳。",
+        "假期全家去海南旅游很开心。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/游.gif`]
+    }
+  },
+  {
+    "池": {
+      "labels": ["会认字"],
+      "组词": ["池塘", "水池", "电池"],
+      "造句": [
+        "小鸭子在池塘里欢快地游来游去。",
+        "遥控器需要两节五号电池。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/池.gif`]
+    }
+  },
+  {
+    "春": {
+      "labels": ["会写字"],
+      "组词": ["春天", "春节", "春风"],
+      "造句": [
+        "春天来了，小草从土里探出头来。",
+        "我们全家欢欢喜喜过春节。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/春.gif`]
+    }
+  },
+  {
+    "风": {
+      "labels": ["会写字"],
+      "组词": ["大风", "风筝", "风景"],
+      "造句": [
+        "今天刮大风，树叶哗哗作响。",
+        "周末我们去公园放风筝。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/风.gif`]
+    }
+  },
+  {
+    "花": {
+      "labels": ["会写字"],
+      "组词": ["花朵", "花园", "花生"],
+      "造句": [
+        "五颜六色的花朵在微风中摇曳。",
+        "奶奶在院子里种了很多花生。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/花.gif`]
+    }
+  },
+  {
+    "入": {
+      "labels": ["会写字"],
+      "组词": ["入口", "进入", "收入"],
+      "造句": [
+        "博物馆的入口处需要安检。",
+        "爸爸的工作收入很稳定。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/入.gif`]
+    }
+  },
+  {
+    "冬": {
+      "labels": ["会写字"],
+      "组词": ["冬天", "冬至", "冬瓜"],
+      "造句": [
+        "冬天我们要穿厚厚的棉衣。",
+        "冬至这天北方人习惯吃饺子。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/冬.gif`]
+    }
+  },
+  {
+    "雪": {
+      "labels": ["会写字"],
+      "组词": ["雪花", "雪人", "滑雪"],
+      "造句": [
+        "晶莹的雪花从天空飘落下来。",
+        "寒假我们一起去滑雪场玩。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/雪.gif`]
+    }
+  },
+  {
+    "飞": {
+      "labels": ["会写字"],
+      "组词": ["飞机", "飞翔", "飞鸟"],
+      "造句": [
+        "天空中有一架喷气式飞机飞过。",
+        "小鸟在蓝天自由自在地飞翔。"
+      ],
+                  "imageUrls": [`${process.env.IMAGE_BASE_PATH}/subjects/yuwen/1b/unit1/lesson1/飞.gif`]
+    }
+  }
+]
           },
           "姓氏歌": {
             type: 'lesson',
@@ -54,3 +209,6 @@ const data = {
 }
 
 export default data;
+
+// lesson1: 会认字：霜，吹，落，降，飘，游，池 会写字：春，风，花，入，冬，雪，飞
+//
