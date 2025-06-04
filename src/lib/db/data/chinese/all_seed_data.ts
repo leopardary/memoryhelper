c68c2cc3685eba85a7119e4350aaf3bf32a47642
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-06-03 22:07:54 -0700

    adding data for yuwen 1a

--- src/lib/db/data/chinese/all_seed_data.ts
+++ src/lib/db/data/chinese/all_seed_data.ts
@@ -1,3 +1,12 @@
+import data_1a1 from './seed_1a/seed_1a1'
+import data_1a2 from './seed_1a/seed_1a2'
+import data_1a3 from './seed_1a/seed_1a3'
+import data_1a4 from './seed_1a/seed_1a4'
+import data_1a5 from './seed_1a/seed_1a5'
+import data_1a6 from './seed_1a/seed_1a6'
+import data_1a7 from './seed_1a/seed_1a7'
+import data_1a8 from './seed_1a/seed_1a8'
+
 import data_1b1 from './seed_1b/seed_1b1'
 import data_1b2 from './seed_1b/seed_1b2'
 import data_1b3 from './seed_1b/seed_1b3'
@@ -17,6 +26,14 @@ import data_4a7 from './seed_4a/seed_4a7'
 import data_4a8 from './seed_4a/seed_4a8'
 
 const data = {
+  data_1a1: data_1a1,
+  data_1a2: data_1a2,
+  data_1a3: data_1a3,
+  data_1a4: data_1a4,
+  data_1a5: data_1a5,
+  data_1a6: data_1a6,
+  data_1a7: data_1a7,
+  data_1a8: data_1a8,
   data_1b1: data_1b1,
   data_1b2: data_1b2,
   data_1b3: data_1b3,
