3542469949a14638a521c12364c160ae8cf6244f
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-06-29 14:31:58 -0700

    improve the bell icon and dropdown

--- src/app/components/Dropdown.tsx
+++ src/app/components/Dropdown.tsx
@@ -1,30 +1,41 @@
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
-import {
-  ArrowPathIcon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-} from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import ClientLink from '@/app/components/ClientLink'
+import { FC } from 'react';
 
-const solutions = [
-  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'Security', description: "Your customers' data will be safe and secure", href: '#', icon: FingerPrintIcon },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
-]
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-]
+// Reference：https://tailwindcss.com/plus/ui-blocks/marketing/elements/flyout-menus
 
-export default function Dropdown() {
+interface DropdownButton {
+  title?: 'string'
+  icon?: FC;
+}
+
+interface PopupOption {
+  name: string,
+  description?: string,
+  href: string,
+  icon?: FC
+}
+
+interface QuickAction {
+  name: string,
+  href: string,
+  icon?: FC
+}
+
+interface DropdownProps {
+  button: DropdownButton;
+  popupOptions?: PopupOption[];
+  quickActions?: QuickAction[];
+}
+
+export default function Dropdown(props: DropdownProps) {
+  const { button, popupOptions, quickActions } = props;
   return (
     <Popover className="relative">
       <PopoverButton className="inline-flex items-center gap-x-1 text-sm/6 font-semibold">
-        <span>Solutions</span>
+        {button.title && <span>{button.title}</span>}
+        {button.icon && <button.icon className="h-6 w-6" />}
         <ChevronDownIcon aria-hidden="true" className="size-5" />
       </PopoverButton>
 
@@ -34,29 +45,26 @@ export default function Dropdown() {
       >
         <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-popover text-sm/6 shadow-lg ring-1 ring-border">
           <div className="p-4">
-            {solutions.map((item) => (
+            {popupOptions?.map((item) => (
               <div key={item.name} className="group relative flex gap-x-6 rounded-lg p-4 transition-colors hover:bg-muted">
                 <div className="mt-1 flex size-11 flex-none items-center justify-center rounded-lg bg-muted group-hover:bg-accent">
-                  <item.icon aria-hidden="true" className="size-6 text-muted-foreground group-hover:text-primary" />
+                  {item.icon && <item.icon aria-hidden="true" className="size-6 text-muted-foreground group-hover:text-primary" />}
                 </div>
                 <div>
-                  <a href={item.href} className="font-semibold text-foreground hover:underline">
-                    {item.name}
-                    <span className="absolute inset-0" />
-                  </a>
+                  <ClientLink href={item.href} className="font-semibold text-foreground hover:underline" text={item.name}><span className="absolute inset-0" /></ClientLink>
                   <p className="mt-1 text-muted-foreground">{item.description}</p>
                 </div>
               </div>
             ))}
           </div>
           <div className="grid grid-cols-2 divide-x divide-border bg-muted">
-            {callsToAction.map((item) => (
+            {quickActions?.map((item) => (
               <a
                 key={item.name}
                 href={item.href}
                 className="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-foreground hover:bg-accent"
               >
-                <item.icon aria-hidden="true" className="size-5 flex-none text-muted-foreground group-hover:text-primary" />
+                {item.icon && <item.icon aria-hidden="true" className="size-5 flex-none text-muted-foreground group-hover:text-primary" />}
                 {item.name}
               </a>
             ))}
