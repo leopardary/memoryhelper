93e43f41ae8af42140181817c40885940994a6bc
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-07-05 22:56:12 -0700

    fix dropdown width/position

--- src/app/components/Dropdown.tsx
+++ src/app/components/Dropdown.tsx
@@ -45,14 +45,14 @@ export default function Dropdown(props: DropdownProps) {
       <PopoverButton className="inline-flex items-center gap-x-1 text-sm/6 font-semibold">
         {button.title && <span className='font-semibold'>{button.title}</span>}
         {button.icon}
-        <ChevronDownIcon aria-hidden="true" className="size-5 fill-muted-foreground" />
+        {/* <ChevronDownIcon aria-hidden="true" className="size-5 fill-muted-foreground" /> */}
       </PopoverButton>
 
       <PopoverPanel
         transition
-        className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
+        className="absolute left-16 z-10 mt-5 flex w-screen max-w-max -translate-x-full px-4 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
       >
-        <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-popover text-sm/6 shadow-lg ring-1 ring-border">
+        <div className="max-w-fit flex-auto overflow-hidden rounded-3xl bg-popover text-sm/6 shadow-lg ring-1 ring-border">
           <div className="p-4">
             {popupOptions?.map((item) => (
               <div key={item.name} className="group relative flex gap-x-6 rounded-lg p-4 transition-colors hover:bg-muted">
@@ -60,7 +60,7 @@ export default function Dropdown(props: DropdownProps) {
                   {item.icon && <item.icon aria-hidden="true" className="size-6 text-muted-foreground group-hover:text-primary" />}
                 </div>
                 <div>
-                  {item.href ? <ClientLink href={item.href} className="font-semibold text-foreground hover:underline font-semibold" text={item.name}><span className="absolute inset-0" /></ClientLink> : <button className="font-semibold text-foreground hover:underline" onClick={item.onClick}>{item.name}<span className="absolute inset-0" /></button>}
+                  {item.href ? <ClientLink href={item.href} className="font-semibold text-foreground hover:underline" text={item.name}><span className="absolute inset-0" /></ClientLink> : <button className="font-semibold text-foreground hover:underline" onClick={item.onClick}>{item.name}<span className="absolute inset-0" /></button>}
                   <p className="mt-1 text-muted-foreground font-semibold">{item.description}</p>
                 </div>
               </div>
