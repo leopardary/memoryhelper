a657620456c5d6454b5d3fe9aeb6808cb7db2fad
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-07-05 23:26:11 -0700

    improve herocard and memorypiececard

--- src/app/components/MemoryPieceCard.tsx
+++ src/app/components/MemoryPieceCard.tsx
@@ -1,13 +1,16 @@
 import { MemoryPieceProps } from '@/lib/db/model/types/MemoryPiece.types';
 import Link from "next/link";
 import Image from "next/image";
+import Badge from '@/app/components/Badge';
 
 export default function MemoryPieceCard({ memoryPiece }: {memoryPiece: MemoryPieceProps}) {
   const { _id, description, content, labels, imageUrls } = memoryPiece;
+  const [wordCombinations, sentenceSamples] = description?.split('##');
+
   return (
     <Link
       href={"/memorypiece/" + _id?.toString()}
-      className="card w-full bg-base-100 hover:shadow-xl transition-shadow"
+      className="w-full rounded-lg hover:border-2 bg-base-100 hover:shadow-xl transition-shadow"
     >
       <div className="m-4">
         <figure>
@@ -20,16 +23,17 @@ export default function MemoryPieceCard({ memoryPiece }: {memoryPiece: MemoryPie
           />
         </figure>
       </div>
-      <div className="card-body">
-        <h2 className="card-title">
+      <div className="flex flex-col items-center">
+        <h2 className="m-2 text-bold">
           {content}
         </h2>
-        <p>{description}</p>
         <div className="flex flex-wrap gap-2 mt-2">
           {labels && labels.map((label, index) => (
-            <div key={index} className="badge badge-outline">{label}</div>
+            <Badge key={index} text={label} />
           ))}
         </div>
+        <p className="m-2">{`词语: ${wordCombinations}`}</p>
+        <p className="m-2">{`例句: ${sentenceSamples}`}</p>
       </div>
     </Link>
   );
