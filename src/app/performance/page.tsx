db8eb62951dc979055c6acf2d60cc314831cc968
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-07-07 21:56:19 -0700

    adding interface defs

--- src/app/performance/page.tsx
+++ src/app/performance/page.tsx
@@ -1,4 +1,4 @@
-import Dashboard from '@/app/components/Dashboard/Dashboard'
+import Dashboard, {SubscriptionOverallRecord} from '@/app/components/Dashboard/Dashboard'
 import { authOptions } from "@/lib/utils/authOptions";
 import { getSubscriptionWithMemoryPiecesAndChecksForUser } from '@/lib/db/api/subscription'
 import { getServerSession } from "next-auth/next";
@@ -8,37 +8,35 @@ export default async function PerformanceDashboard() {
   const user = session?.user;
   const subscriptions = await getSubscriptionWithMemoryPiecesAndChecksForUser(user.id);
 
-  const plainObj = Object.keys(subscriptions).reduce((obj, subscriptionId) => {
+  const plainObj: Record<string, SubscriptionOverallRecord> = Object.keys(subscriptions).reduce((obj, subscriptionId) => {
     const rawSubscription = subscriptions[subscriptionId];
-    obj[subscriptionId] = {};
-    obj[subscriptionId]['subscription'] = {};
-    obj[subscriptionId]['subscription']['userId'] = rawSubscription['subscription'].userId.toString();
-    obj[subscriptionId]['subscription']['memoryPieceId'] = rawSubscription['subscription'].memoryPieceId.toString();
-    obj[subscriptionId]['subscription']['status'] = rawSubscription['subscription'].status;
-    obj[subscriptionId]['subscription']['easeFactor'] = rawSubscription['subscription'].easeFactor;
-    obj[subscriptionId]['subscription']['currentInterval'] = rawSubscription['subscription'].currentInterval;
-    obj[subscriptionId]['subscription']['nextTestDate'] = rawSubscription['subscription'].nextTestDate;
-
-    obj[subscriptionId]['memoryPiece'] = {};
-    obj[subscriptionId]['memoryPiece']['id'] = rawSubscription['memoryPiece'].id;
-    obj[subscriptionId]['memoryPiece']['content'] = rawSubscription['memoryPiece'].content;
-    obj[subscriptionId]['memoryPiece']['imageUrls'] = rawSubscription['memoryPiece'].imageUrls;
-    obj[subscriptionId]['memoryPiece']['description'] = rawSubscription['memoryPiece'].description;
-    obj[subscriptionId]['memoryPiece']['labels'] = rawSubscription['memoryPiece'].labels;
-
-    obj[subscriptionId]['memoryChecks'] = [];
-    for (const check of rawSubscription['memoryChecks']) {
-      const checkPlainObj = {};
-      checkPlainObj['id'] = check.id;
-      checkPlainObj['subscriptionId'] = check.subscription.toString();
-      checkPlainObj['score'] = check.score;
-      checkPlainObj['createdAt'] = check.createdAt;
-      checkPlainObj['updatedAt'] = check.updatedAt;
-      obj[subscriptionId]['memoryChecks'].push(checkPlainObj);
-    }
+    obj[subscriptionId] = {
+      subscription: {
+        userId: rawSubscription['subscription'].userId.toString(),
+        memoryPieceId: rawSubscription['subscription'].memoryPieceId.toString(),
+        status: rawSubscription['subscription'].status,
+        easeFactor: rawSubscription['subscription'].easeFactor,
+        currentInterval: rawSubscription['subscription'].currentInterval,
+        nextTestDate: rawSubscription['subscription'].nextTestDate
+      }, 
+      memoryPiece: {
+        id: rawSubscription['memoryPiece'].id,
+        content: rawSubscription['memoryPiece'].content,
+        imageUrls: rawSubscription['memoryPiece'].imageUrls,
+        description: rawSubscription['memoryPiece'].description,
+        labels: rawSubscription['memoryPiece'].labels
+      }, 
+      memoryChecks: rawSubscription['memoryChecks']?.map((memoryCheck: any) => {
+        return {
+          id: memoryCheck.id,
+          subscriptionId: memoryCheck.subscription.toString(),
+          score: memoryCheck.score,
+          createdAt: memoryCheck.createdAt,
+          updatedAt: memoryCheck.updatedAt
+        }
+      })} as SubscriptionOverallRecord;
     return obj;
   }, {});
 
-  console.log(plainObj);
   return (<div className="flex flex-col items-center"><Dashboard record={plainObj}/></div>)
 }
