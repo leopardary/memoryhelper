643b0c95e19c03d87907ffd3d07c013c533a2cd8
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-07-12 19:07:02 -0700

    adding name in front of the bell icon on nav bar

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -19,7 +19,13 @@ export default async function Navbar() {
       <div className="w-full max-w-7xl flex flex-row gap-2 justify-between font-serif sticky top-0 bg-background z-10">
         <Logo />
         <div className="my-1 mx-3 flex flex-row gap-2 items-center">
-          {session && <UserSessionDropDown/>}
+          {session && 
+          <div className='flex flex-row items-center'>
+            <span className="text-base/7 font-semibold text-foreground mr-9">
+            {`Hi, ${session.user.name.split(' ')[0]}`}
+            </span>
+            <UserSessionDropDown/>
+          </div>}
           <UserMenuButton session={session} />
         </div>
       </div>
