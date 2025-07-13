f645fd4e3177b66f4de5208c4764e25ad1498aeb
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-07-12 17:40:46 -0700

    improve the signup/signin form

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { signIn } from "next-auth/react";
 import { useState } from "react";
+import { Field, Fieldset, Input, Label, Legend } from '@headlessui/react'
+import { Button } from '@/app/components/button';
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
@@ -18,19 +20,34 @@ export default function SignInPage() {
   };
 
   return (
-    <form className="w-full flex flex-col items-center m-2" onSubmit={handleSubmit}>
-      <fieldset className="fieldset w-full">
-          <legend className="fieldset-legend">Email: </legend>
-          <input type="email" className="input w-full" value={email} onChange={e => setEmail(e.target.value)} />
-        </fieldset>
-        <fieldset className="fieldset w-full">
-          <legend className="fieldset-legend">Password: </legend>
-          <input type="password" className="input w-full" value={password} onChange={e => setPassword(e.target.value)} />
-        </fieldset>
-      <div className="w-full flex justify-between mt-4">
-          <button className="btn btn-secondary" type="reset" onClick={handleReset}>Reset</button>
-          <button className="btn btn-primary" type="submit">Sign In</button>
-        </div>
-    </form>
+    <div className="w-full justify-items-center px-4">
+      <div className="w-full md:w-10/12 lg:w-8/12 xl:w-6/12">
+        <Fieldset className="space-y-6 rounded-xl bg-gray-100 dark:bg-gray-900 p-6 sm:p-10">
+          <Legend className="text-base/7 font-semibold text-foreground border-b">Sign In</Legend>
+          <Field>
+            <Label className="text-sm/6 font-medium text-foreground">Email</Label>
+            <Input
+              type="email"
+              className='mt-3 block w-full rounded-lg border-none bg-gray-900/5 dark:bg-gray-100/5 px-3 py-1.5 text-sm/6 text-foreground focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </Field>
+          <Field>
+            <Label className="text-sm/6 font-medium text-foreground">Password</Label>
+            <Input
+              type="password"
+              className='mt-3 block w-full rounded-lg border-none bg-gray-900/5 dark:bg-gray-100/5 px-3 py-1.5 text-sm/6 text-foreground focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </Field>
+          <div className="w-full flex justify-between mt-4">
+            <Button variant="outline" type="reset" onClick={handleReset}>Reset</Button>
+            <Button type="submit" onClick={handleSubmit}>Sign In</Button>
+          </div>
+        </Fieldset>
+      </div>
+    </div>
   );
 }
