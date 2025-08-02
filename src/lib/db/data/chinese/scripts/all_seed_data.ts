87e37353c9e8f9eb7cabc5bb4d80f73767829db9
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-08-02 10:08:28 -0700

    move the scripts to script folder

---
+++ src/lib/db/data/chinese/scripts/all_seed_data.ts
@@ -0,0 +1,55 @@
+import data_1a1 from '../seed_1a/seed_1a1'
+import data_1a2 from '../seed_1a/seed_1a2'
+import data_1a3 from '../seed_1a/seed_1a3'
+import data_1a4 from '../seed_1a/seed_1a4'
+import data_1a5 from '../seed_1a/seed_1a5'
+import data_1a6 from '../seed_1a/seed_1a6'
+import data_1a7 from '../seed_1a/seed_1a7'
+import data_1a8 from '../seed_1a/seed_1a8'
+
+import data_1b1 from '../seed_1b/seed_1b1'
+import data_1b2 from '../seed_1b/seed_1b2'
+import data_1b3 from '../seed_1b/seed_1b3'
+import data_1b4 from '../seed_1b/seed_1b4'
+import data_1b5 from '../seed_1b/seed_1b5'
+import data_1b6 from '../seed_1b/seed_1b6'
+import data_1b7 from '../seed_1b/seed_1b7'
+import data_1b8 from '../seed_1b/seed_1b8'
+
+import data_4a1 from '../seed_4a/seed_4a1'
+import data_4a2 from '../seed_4a/seed_4a2'
+import data_4a3 from '../seed_4a/seed_4a3'
+import data_4a4 from '../seed_4a/seed_4a4'
+import data_4a5 from '../seed_4a/seed_4a5'
+import data_4a6 from '../seed_4a/seed_4a6'
+import data_4a7 from '../seed_4a/seed_4a7'
+import data_4a8 from '../seed_4a/seed_4a8'
+
+const data = {
+  data_1a1: data_1a1,
+  data_1a2: data_1a2,
+  data_1a3: data_1a3,
+  data_1a4: data_1a4,
+  data_1a5: data_1a5,
+  data_1a6: data_1a6,
+  data_1a7: data_1a7,
+  data_1a8: data_1a8,
+  data_1b1: data_1b1,
+  data_1b2: data_1b2,
+  data_1b3: data_1b3,
+  data_1b4: data_1b4,
+  data_1b5: data_1b5,
+  data_1b6: data_1b6,
+  data_1b7: data_1b7,
+  data_1b8: data_1b8,
+  data_4a1: data_4a1,
+  data_4a2: data_4a2,
+  data_4a3: data_4a3,
+  data_4a4: data_4a4,
+  data_4a5: data_4a5,
+  data_4a6: data_4a6,
+  data_4a7: data_4a7,
+  data_4a8: data_4a8,
+}
+
+export default data;
