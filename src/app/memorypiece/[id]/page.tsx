55df8c3e56e881de9ee42c84f58a6a29d67ba0d1
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-08-02 11:47:03 -0700

    render related units under memorypiece card

--- src/app/memorypiece/[id]/page.tsx
+++ src/app/memorypiece/[id]/page.tsx
@@ -1,31 +1,41 @@
-// import Breadcrumbs from "@/app/components/Breadcrumbs"
-// import { parentUnitChain } from "@/lib/db/api/unit" 
+import Breadcrumbs from "@/app/components/Breadcrumbs"
+import { parentUnitChain } from "@/lib/db/api/unit"
+import { getSubject } from "@/lib/db/api/subject"
 import { getMemoryPiece } from '@/lib/db/api/memory-piece'
 import {HeroCard} from '@/app/components/HeroCard'
 
-// function getSubjectTitle(memoryPiece: any) {
-//   return memoryPiece?.subject.title;
-// }
+const renderUnit = async (unit: any) => {
+    const unitChain = await parentUnitChain(unit.id.toString());
+    const breadcrumbsSegments = unitChain?.reverse().map(unit => {return {name: unit.title, url: `/unit/${unit.id.toString()}`};}) || [];
+    const subject = await getSubject(unit.subject._id.toString());
+    if (subject?.title == null) {
+      return <></>;
+    }
+    breadcrumbsSegments.unshift({name: subject.title, url: `/subject/${subject.id.toString()}`});
+    
+    return (
+      <div key={unit.id} className="space-y-2">
+        <Breadcrumbs segments={breadcrumbsSegments}/>
+      </div>
+    );
+};
 
 export default async function MemoryPiece({params}: {params: Promise<{id: string}>}) {
   const { id } = await params;
   const memoryPieceId = id;
   const memoryPiece = await getMemoryPiece(memoryPieceId);
   if (!memoryPiece) throw new Error(`memoryPiece not found.`);
-  // get all the units from the memoryPiece's unit upto the root unit.
-  // const unitChain = memoryPiece?.units?.[0] && await parentUnitChain(memoryPiece.units[0].id.toString());
-  // const breadcrumbsSegments = unitChain?.reverse().map(unit => {return {name: unit.title, url: `/unit/${unit.id}`};}) || [];
-  // // append with the current memoryPiece.
-  // breadcrumbsSegments.push({name: memoryPiece.content, url: `/memorypiece/${memoryPiece.id}`});
-  // // prefix with the subject.
-  // breadcrumbsSegments.unshift({name: getSubjectTitle(memoryPiece), url: `/subject/${memoryPiece?.subject.id}`});
-  // // prefix with home.
-  // breadcrumbsSegments.unshift({name: 'Home', url: `/`});
 
   return <>
   {/* <Breadcrumbs segments={breadcrumbsSegments}/> */}
   <div className="flex flex-col items-center">
-  <HeroCard imageSrcs={memoryPiece.imageUrls} imageAlt={memoryPiece.content} title={memoryPiece.content} description={memoryPiece.description} href={`/memorypiece/${memoryPiece.id}`} />
+    <HeroCard imageSrcs={memoryPiece.imageUrls} imageAlt={memoryPiece.content} title={memoryPiece.content} description={memoryPiece.description} href={`/memorypiece/${memoryPiece.id}`} />
+  </div>
+  <div>
+    <h2 className="text-2xl font-bold mt-6">Related Units</h2>
+    <div className="space-y-4">
+      {memoryPiece.units?.map(unit => renderUnit(unit))}
+    </div>
   </div>
   </>
 }
