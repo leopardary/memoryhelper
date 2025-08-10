9ff965b39275c448dee7d17e29b7201a98654804
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-08-09 22:27:32 -0700

    add support for adding root units

--- src/app/subject/[id]/page.tsx
+++ src/app/subject/[id]/page.tsx
@@ -1,10 +1,19 @@
 import Breadcrumbs from "@/app/components/Breadcrumbs"
-import { getDirectChildrenBySubject } from "@/lib/db/api/unit" 
+import { getDirectChildrenBySubject, addRootUnitForSubject } from "@/lib/db/api/unit" 
 import UnitCard from "@/app/components/UnitCard"
 import {getSubject} from "@/lib/db/api/subject"
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/lib/utils/authOptions";
+import AddRootUnitModal from '@/app/components/AddRootUnitModal'
 
 export default async function Subject({params}: {params: Promise<{id: string}>}) {
   const { id } = await params;
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  let editMode = false;
+  if (user?.name == "Wenjiao Wang") {
+    editMode = true;
+  }
   const subjectId = id;
   const units = await getDirectChildrenBySubject(subjectId)
   const subject = await getSubject(subjectId);
@@ -13,6 +22,7 @@ export default async function Subject({params}: {params: Promise<{id: string}>})
   }
   const breadcrumbs = [{url: `/`, name: 'Home'}, {url: `/subject/${subjectId}`, name: subject.title}]
   return <>
+  {editMode && <div className='w-full flex flex-row justify-center'><AddRootUnitModal subjectTitle={subject.title} subjectId={subjectId} addRootUnitForSubject={addRootUnitForSubject} /></div>}
   <Breadcrumbs segments={breadcrumbs}/>
   <div className="divider"></div>
   <div className="flex flex-col items-center">
