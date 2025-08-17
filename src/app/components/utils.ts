5324bb2b7fc83be5c1bce22ee387f5160e66ca7b
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-08-17 12:09:58 -0700

    improve utils to include handleRead

--- src/app/components/utils.ts
+++ src/app/components/utils.ts
@@ -1,6 +1,22 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+export const DESCRIPTION_SEPARATOR = '##';
+export const SENTENCE_SEPARATOR = '/';
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const handleRead = async (text: string) => {
+  const res = await fetch("/api/read-text", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text }),
+  });
+
+  const data = await res.json();
+  const url = data?.url;
+  const audio = new Audio(url);
+  audio.play();
+};
