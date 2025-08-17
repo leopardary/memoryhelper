0131509f365272c6f667516a09b384f65cfdf800
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-08-17 12:06:07 -0700

    improve HeroCard to support read

--- src/app/components/HeroCard.tsx
+++ src/app/components/HeroCard.tsx
@@ -1,7 +1,10 @@
+"use client"
 import Link from "next/link";
 import Image from "next/image";
 import isEmpty from 'lodash/isEmpty';
 import { Button } from '@/app/components/button';
+import {DESCRIPTION_SEPARATOR, SENTENCE_SEPARATOR} from '@/app/components/utils';
+import { SpeakerWaveIcon } from "@heroicons/react/24/outline";
 
 export interface HeroCardProps {
   imageSrcs?: string[];
@@ -9,27 +12,59 @@ export interface HeroCardProps {
   title?: string;
   description?: string;
   href: string;
-  buttonContent?: string
+  buttonContent?: string;
+  // The memoryPiece will be masked if testMode is true.
+  testMode?: boolean;
 }
 
+const hideKeyWord = (keyWord: string, content: string) => content.replaceAll(keyWord, '▢');
+
+const ImageSize = 400;
+
+const ImageMask = () => <div className="w-[400px] h-[400px] bg-muted rounded-lg animate-pulse"></div>
+
+
+
 export function HeroCard(props: HeroCardProps) {
-  const {imageSrcs, imageAlt, title, description, href, buttonContent} = props;
-  const [wordCombinations, sentenceSamples] = description == null ? ['', ''] : description?.split("##");
+  const {imageSrcs, imageAlt, title, description, href, buttonContent, testMode} = props;
+  const [wordCombinations, sentenceSamples] = description == null ? ['', ''] : description?.split(DESCRIPTION_SEPARATOR);
+  const sentences = sentenceSamples?.split(SENTENCE_SEPARATOR);
+    const handleRead = async (text: string) => {
+    const res = await fetch("/api/read-text", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text }),
+    });
+    const data = await res.json();
+    const audio = new Audio(data?.url);
+    audio.play();
+  };
+  const ReadContentIcon = (content: string) => <SpeakerWaveIcon className="ml-2 size-6 flex-none rounded-lg bg-muted group-hover:bg-accent hover:cursor-pointer" onClick={() => handleRead(content)}/>
   return (
   <div className="w-full border-2 md:border-4 rounded-lg">
           <div className="m-2 md:m-4 flex flex-col md:flex-row items-center">
-            <Image
+            {testMode ? <ImageMask /> : <Image
               src={imageSrcs?.[0] || ""}
               alt={imageAlt}
-              width={400}
-              height={800}
+              width={ImageSize}
+              height={ImageSize}
               className="w-full max-w-sm rounded-lg shadow-xl"
               priority
-            />
+            />}
             <div className="m-2 md:items-baseline">
-              <h1 className="text-5xl font-bold font-serif">{title}</h1>
-              <p className="ml-2 pt-6 italic text-muted-foreground">{wordCombinations}</p>
-              <p className="ml-2 pt-2 italic text-muted-foreground">{sentenceSamples}</p>
+              <div className="flex flex-row items-center">
+                <h1 className="text-5xl font-bold font-serif">{testMode ? hideKeyWord(title || '', title || '') : title}</h1>{ReadContentIcon(title||'')}
+              </div>
+              <div className="flex flex-row ml-2 pt-6">
+              <p className="italic text-muted-foreground">{testMode ? hideKeyWord(title || '', wordCombinations) : wordCombinations}</p>
+              {ReadContentIcon(wordCombinations||'')}
+              </div>
+              {sentences?.map((content: string) => 
+                (<div key={content} className="flex flex-row ml-2 pt-2">
+                <p className="italic text-muted-foreground">{`${testMode ? hideKeyWord(title || '', content) : content}`}</p>
+                {ReadContentIcon(content||'')}
+                </div>)
+                )}
               {!isEmpty(buttonContent) && <Link
                 href={href}
                 className="btn-primary btn"
