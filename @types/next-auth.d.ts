3a768d348a543992a8f877366431f5914193cdef
Wenjiao Wang <wenjiao.wang1@gmail.com>
2025-10-08 07:10:08 -0700

    update next-auth

--- @types/next-auth.d.ts
+++ @types/next-auth.d.ts
@@ -1,7 +1,12 @@
+import { Permission } from "@/lib/db/model/types/Role.types";
+
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+      defaultRole?: string;
+      isAdmin?: boolean;
+      permissions?: Permission[];
     } & DefaultSession["user"];
   }
 
@@ -13,6 +18,7 @@ declare module "next-auth" {
 declare module "next-auth/jwt" {
   interface JWT {
     id: string;
+    defaultRole?: string;
   }
 }
 
